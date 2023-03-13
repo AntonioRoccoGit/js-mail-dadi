bb88eff1c769e0dd247f9966e71ef57a94fa6966
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-03-13 14:25:10 +0100

    end project

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -8,14 +8,25 @@ console.log(resault.classList);
 //call to action 
 const callToAction = document.getElementById("call-to-action");
 console.log(callToAction); 
-// number generator
-let computerDiceNumber = Math.floor(Math.random() * 6 + 1);
-let userDiceNumber = Math.floor(Math.random() * 6 + 1);
-console.log(computerDiceNumber, userDiceNumber);
-//inseriamo il valore dei dadi nel nonsto documento
-computerDiceElem.innerHTML = computerDiceNumber;
-userDiceElem.innerHTML += userDiceNumber;
-//verifichaimo la condizione di vincita
-if (userDiceNumber > computerDiceNumber){
-    resault.innerHTML = "Complimenti hai vinto"
-}
+
+//qui avevo due soluzioni per rendere il tutto interattivo
+//potevamo assegnare un reload al call to action per refreshare la pagina
+//possiamo creare una function da assegare al call to action
+//procederemo con la strada apparentemente piu complicata, ma sicuramente piu ottimizzata
+
+callToAction.addEventListener("click", function(){
+    // number generator
+    let computerDiceNumber = Math.floor(Math.random() * 6 + 1);
+    let userDiceNumber = Math.floor(Math.random() * 6 + 1);
+    //inseriamo il valore dei dadi nel nonsto documento
+    computerDiceElem.innerHTML = computerDiceNumber;
+    userDiceElem.innerHTML = `Tuo Dado <br> ${userDiceNumber}`;
+    //verifichaimo la condizione di vincita
+    if (userDiceNumber > computerDiceNumber){
+        resault.innerHTML = "Complimenti hai vinto";
+    }else {
+        resault.innerHTML = "Non hai avuto fortuna";
+    }
+    //tiriamo fuori il risultato nascosto
+    resault.classList.remove("d-none");
+});
